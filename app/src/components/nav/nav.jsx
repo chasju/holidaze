1f6dfe6d6e1db6949a098bd129a69a9b4d2a8f26
chasju <91468816+chasju@users.noreply.github.com>
2023-05-03 10:37:05 +0200

    Add nav skeleton

--- app/src/components/nav/nav.jsx
+++ app/src/components/nav/nav.jsx
@@ -8,19 +8,20 @@ const NavComponent = () => {
     <div>
       <Navbar expand="lg">
         <Container>
-          <Navbar.Brand href="#home">Holidaze</Navbar.Brand>
+          <Navbar.Brand href="/">Holidaze</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="/">HOME</Nav.Link>
+              <Nav.Link href="/bookings">MY BOOKINGS</Nav.Link>
+              <Nav.Link href="/venues">MY VENUES</Nav.Link>
+              <Nav.Link href="/profile">MY PROFILE</Nav.Link>
+              <Nav.Link href="/login">LOGIN</Nav.Link>
+              <Nav.Link href="/register">REGISTER</Nav.Link>
+              <Nav.Link href="/">LOGOUT</Nav.Link>
+            </Nav>
+            <Nav className="me-auto">
+              <Nav.Link href="/create/venue">List your venue</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
