fef78fe9475c469b514b2281cfcecef3f2f3892d
chasju <91468816+chasju@users.noreply.github.com>
2023-05-04 10:34:31 +0200

    Change link position, add aria-label

--- app/src/components/card/Card.jsx
+++ app/src/components/card/Card.jsx
@@ -8,28 +8,28 @@ const CardComponent = () => {
   return (
     <div className="mt-5">
       <Container>
-        <Link href="/venues" className="text-decoration-none">
-          <Card className="border-0 shadow">
-            <Card.Img variant="top" src="picture.jpg" />
-            <Card.Body className="d-flex justify-content-between">
-              <div>
-                <Card.Title>
-                  <h2 className="fs-4">Card Title</h2>
-                </Card.Title>
-                <Card.Text>Price</Card.Text>
-              </div>
-              <div>
-                <Card.Text className="position-relative d-flex justify-content-center align-items-center" style={{ width: 35, height: 35 }}>
-                  <FontAwesomeIcon icon={faUser} />
-                  <span className="position-absolute top-0 start-0">2</span>
-                </Card.Text>
-              </div>
-            </Card.Body>
-            <Button variant="primary" className="bg-secondary shadow border-0 mb-4">
-              View venue
-            </Button>
-          </Card>
-        </Link>
+        <Card className="border-0 shadow">
+          <Link href="/venues" className="text-decoration-none">
+            <Card.Img variant="top" src="picture.jpg" alt="House" />
+          </Link>
+          <Card.Body className="d-flex justify-content-between">
+            <div>
+              <Card.Title>
+                <h2 className="fs-4">Card Title</h2>
+              </Card.Title>
+              <Card.Text aria-label="Price per night">Price</Card.Text>
+            </div>
+            <div>
+              <Card.Text aria-label="Max number of people" className="position-relative d-flex justify-content-center align-items-center" style={{ width: 35, height: 35 }}>
+                <FontAwesomeIcon icon={faUser} />
+                <span className="position-absolute top-0 start-0">2</span>
+              </Card.Text>
+            </div>
+          </Card.Body>
+          <Button href="/venues" variant="primary" className="bg-secondary shadow border-0 mb-4">
+            View venue
+          </Button>
+        </Card>
       </Container>
     </div>
   );
