50299154c6ec951761881fea0caab72692671d13
chasju <91468816+chasju@users.noreply.github.com>
2023-05-06 14:57:52 +0200

    Success message

--- app/src/components/createVenueForm/CreateVenueForm.jsx
+++ app/src/components/createVenueForm/CreateVenueForm.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
 
 const CreateVenueForm = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => {
+    //reset
+    setShow(false);
+  };
+  const handleShow = () => setShow(true);
+
   return (
     <div>
       <Form className="m-auto mt-4" style={{ maxWidth: 500 }}>
@@ -20,9 +30,17 @@ const CreateVenueForm = () => {
           <Form.Label visuallyHidden>max guests</Form.Label>
           <Form.Control type="number" placeholder="Maximum guests" className="border-light shadow py-3" />
         </Form.Group>
-        <Button variant="primary" type="submit" className="w-100 bg-secondary py-3 mt-4 border-0 shadow">
+        <Button onClick={handleShow} variant="primary" type="button" className="w-100 bg-secondary py-3 mt-4 border-0 shadow">
           Create Venue
         </Button>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Body className="rounded-top text-center text-primary fw-bold fs-3 p-5">Venue created!</Modal.Body>
+          <div className=" rounded-bottom d-flex justify-content-center pb-5">
+            <Button variant="secondary" href="/venue">
+              Go to venue
+            </Button>
+          </div>
+        </Modal>
       </Form>
     </div>
   );
