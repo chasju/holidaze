d3fb6fb17f2e3de10c11dceb451d1ea41af2e5b4
chasju <91468816+chasju@users.noreply.github.com>
2023-05-08 17:56:50 +0200

    Create venue responsiveness

--- app/src/pages/create/venue/index.jsx
+++ app/src/pages/create/venue/index.jsx
@@ -4,7 +4,7 @@ import { Card, Container } from "react-bootstrap";
 export default function createVenuePage() {
   return (
     <div>
-      <Container className="mt-5">
+      <Container className="mt-5" style={{ maxWidth: 700 }}>
         <Card className="border-0">
           <h1 className="text-primary fw-bold mb-3">Create Venue</h1>
           <Card.Img src="/picture.jpg" alt="preview uploaded image" />
