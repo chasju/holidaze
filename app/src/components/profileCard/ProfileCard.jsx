e095198661bbc3cff6cf2f66ccfff76e41f7a58d
chasju <91468816+chasju@users.noreply.github.com>
2023-05-24 11:21:04 +0200

    Update info on Profile page based on click

--- app/src/components/profileCard/ProfileCard.jsx
+++ app/src/components/profileCard/ProfileCard.jsx
@@ -3,26 +3,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { Card } from "react-bootstrap";
 
-const ProfileCard = () => {
+const ProfileCard = ({ data }) => {
   return (
     <div className="mt-5 row row-lg-cols-2 gap-5 justify-content-between">
       <Card className="border-0 shadow col-md-5 col-lg " style={{ maxWidth: 550 }}>
-        <Link href="/venue" className="text-decoration-none">
-          <Card.Img variant="top" src="picture.jpg" alt="House" />
+        <Link href={`/venue?id=${data?.id}`} className="text-decoration-none">
+          <Card.Img variant="top" src={data?.media?.[0]} alt={data?.name} />
         </Link>
         <Card.Body className="d-flex justify-content-between">
           <div>
             <Card.Title>
-              <h2 className="fs-4 text-primary">Card Title</h2>
+              <h2 className="fs-4 text-primary">{data?.name}</h2>
             </Card.Title>
             <Card.Text aria-label="Price per night" className="text-secondary">
-              Price
+              {data?.price} kr per night
             </Card.Text>
           </div>
           <div>
             <Card.Text aria-label="Max number of people" className="position-relative d-flex justify-content-center align-items-center" style={{ width: 35, height: 35 }}>
               <FontAwesomeIcon icon={faUser} />
-              <span className="position-absolute top-0 start-0">2</span>
+              <span className="position-absolute top-0 start-0">{data?.maxGuests}</span>
             </Card.Text>
           </div>
         </Card.Body>
--- app/src/pages/profile/index.jsx
+++ app/src/pages/profile/index.jsx
@@ -2,8 +2,27 @@ import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, Container } from "react-bootstrap";
 import ProfileCard from "@/components/profileCard/ProfileCard";
+import { BASE_URL } from "@/utils/baseUrl";
+import { getStorage } from "@/utils/localStorage/getLocalStorage";
+import useGetAuth from "@/hooks/useGetAuth";
+import { useState } from "react";
+import MyBookingsCard from "@/components/myBookingsCard/MybookingsCard";
 
 export default function profilePage() {
+  const profile = getStorage("profile");
+
+  const { data, isLoading, isError } = useGetAuth(`${BASE_URL}/profiles/${profile?.name}?_bookings=true&_venues=true`);
+
+  const venues = data?.venues;
+  const bookings = data?.bookings;
+  console.log(bookings);
+
+  const [active, setActive] = useState("venues");
+
+  const handleChange = (button) => {
+    setActive(button);
+  };
+
   return (
     <Container className="mt-5">
       <h1 className="text-primary">Profile</h1>
@@ -15,17 +34,29 @@ export default function profilePage() {
         <p className="mb-0 text-primary fw-semibold fs-4">Hello Charlie</p>
       </div>
       <div className="w-100 mt-4 d-flex gap-3 overflow-hidden overflow-scroll">
-        <Button variant="secondary" active="true" style={{ width: 150 }} className="flex-shrink-0">
+        <Button variant="secondary" active={active === "venues" ? true : false} onClick={() => handleChange("venues")} style={{ width: 150 }} className="flex-shrink-0">
           My Venues
         </Button>
-        <Button variant="secondary" style={{ width: 150 }} className="flex-shrink-0">
+        <Button variant="secondary" active={active === "bookings" ? true : false} onClick={() => handleChange("bookings")} style={{ width: 150 }} className="flex-shrink-0">
           My Bookings
         </Button>
-        <Button variant="secondary" style={{ width: 150 }} className="flex-shrink-0">
+        <Button variant="secondary" active={active === "info" ? true : false} onClick={() => handleChange("info")} style={{ width: 150 }} className="flex-shrink-0">
           My Info
         </Button>
       </div>
-      <ProfileCard />
+      {active === "venues" &&
+        venues?.map((venue) => {
+          return <ProfileCard key={venue?.id} data={venue} />;
+        })}
+      {active === "bookings" &&
+        bookings?.map((booking) => {
+          return <MyBookingsCard key={booking?.id} data={booking?.venue} dateFrom={booking?.dateFrom} dateTo={booking?.dateTo} />;
+        })}
+      {active === "info" && (
+        <Container className="bg-lighter border-1 p-4 mt-4 rounded-1">
+          <div>email: {profile.email}</div>
+        </Container>
+      )}
     </Container>
   );
 }
