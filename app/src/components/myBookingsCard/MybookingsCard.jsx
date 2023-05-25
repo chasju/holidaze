7b5e8bbb8da2fff55d6a4ae6122f3704870f3813
chasju <91468816+chasju@users.noreply.github.com>
2023-05-25 09:59:44 +0200

    Wrap bookings on small screen

--- app/src/components/myBookingsCard/MybookingsCard.jsx
+++ app/src/components/myBookingsCard/MybookingsCard.jsx
@@ -8,19 +8,21 @@ const MyBookingsCard = ({ data, dateFrom, dateTo }) => {
   const to = formatDate(dateTo);
 
   return (
-    <div className="h-25 d-flex align-items-center gap-4 mt-4">
+    <div className="h-25 bg-lighter rounded-1 p-1 d-flex flex-wrap align-items-center gap-4 mt-4">
       <Link href={`/venue?id=${data?.id}`} className="text-decoration-none">
         <img src={data?.media?.[0]} alt={data?.name} style={{ objectFit: "cover", width: 70, height: 70 }} />
       </Link>
-      <div>
-        <h2 className="fs-4 text-primary">{data?.name}</h2>
-        <div aria-label="Price per night" className="text-secondary">
-          {data?.price} kr per night
+      <div className="d-flex align-items-center gap-4">
+        <div>
+          <h2 className="fs-4 text-primary text-wrap text-break">{data?.name}</h2>
+          <div aria-label="Price per night" className="text-secondary">
+            {data?.price} kr per night
+          </div>
         </div>
-      </div>
-      <div className="ms-auto text-primary p-2 bg-lighter rounded-1">
-        <div>From: {from}</div>
-        <div>To: {to}</div>
+        <div className="ms-auto text-primary p-2 ">
+          <div>From: {from}</div>
+          <div>To: {to}</div>
+        </div>
       </div>
     </div>
   );
