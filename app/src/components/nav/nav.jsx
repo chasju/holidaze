077768dc2c1c90ef2ee84ac6bd4bd83b6949fb3c
chasju <91468816+chasju@users.noreply.github.com>
2023-05-28 14:01:48 +0200

    Conditional rendering logged in

--- app/src/components/nav/nav.jsx
+++ app/src/components/nav/nav.jsx
@@ -16,84 +16,88 @@ const NavComponent = () => {
   const handleIsNavOpen = () => {
     setIsNavOpen(!isNavOpen);
   };
-
-  console.log(isNavOpen);
-
   const handleLogOut = () => {
     removeFromStorage("profile");
     handleIsNavOpen();
+    router.push("/", "/");
   };
 
   const [isLoggedIn, setIsLoggedIn] = useState("false");
   const storageProfile = getStorage("profile");
 
-  // const handleLoggedIn = () => {
-  //   if (storageProfile) {
-  //     console.log("true");
-  //   } else {
-  //     console.log("false");
-  //   }
-  // };
-
-  // handleLoggedIn();
+  useEffect(() => {
+    if (storageProfile) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [storageProfile]);
 
   return (
     <div>
       <Container className="d-lg-flex justify-content-between align-items-center">
         <Navbar expanded={isNavOpen} expand="lg" className="mt-3">
-          <Nav>
-            <Nav.Link href="/create/venue">
-              <div className="bg-secondary shadow-sm p-2 px-4 rounded-1 d-lg-none">
-                <FontAwesomeIcon icon={faPlus} className="text-white" />
-              </div>
-            </Nav.Link>
-          </Nav>
+          {isLoggedIn && (
+            <Nav>
+              <Nav.Link href="/create/venue">
+                <div className="bg-secondary shadow-sm p-2 px-4 rounded-1 d-lg-none">
+                  <FontAwesomeIcon icon={faPlus} className="text-white" />
+                </div>
+              </Nav.Link>
+            </Nav>
+          )}
           <Navbar.Brand href="/" className="text-primary">
             HOLIDAZE
           </Navbar.Brand>
           <Navbar.Toggle onClick={handleIsNavOpen} aria-controls="basic-navbar-nav" className="border-0 d-lg-none" />
           <Navbar.Collapse id="basic-navbar-nav" className={`d-lg-none`}>
             <Nav className="mt-5 d-lg-none">
-              <div>
-                <Nav.Link href="/" className="bg-danger text-primary fw-bold rounded-1 text-center py-3">
-                  HOME
-                </Nav.Link>
-                <Nav.Link href="/bookings" className="bg-dark text-primary fw-bold rounded-1 text-center py-3">
-                  MY BOOKINGS
-                </Nav.Link>
-                <Nav.Link href="/venues" className="bg-light text-primary fw-bold rounded-1 text-center py-3">
-                  MY VENUES
-                </Nav.Link>
-                <Nav.Link href="/profile" className="bg-lighter text-primary fw-bold rounded-1 text-center py-3">
-                  MY PROFILE
-                </Nav.Link>
-              </div>
-              <div>
-                <Nav.Link href="/login" className="bg-danger text-primary fw-bold rounded-1 text-center py-3">
-                  LOGIN
-                </Nav.Link>
-                <Nav.Link href="/register " className="bg-dark text-primary fw-bold rounded-1 text-center py-3">
-                  REGISTER
-                </Nav.Link>
-                <Nav.Link onClick={handleLogOut} className="w-100 bg-light text-primary fw-bold border-0 rounded-1 text-center py-3">
-                  LOG OUT
-                </Nav.Link>
-              </div>
-            </Nav>
-            <Nav className="mt-5 d-lg-none">
-              <Nav.Link href="/create/venue" className="bg-danger text-white fw-bold rounded-1 text-center py-3">
-                List your venue
-              </Nav.Link>
+              {isLoggedIn && (
+                <div>
+                  <Nav.Link href="/" className="bg-danger text-primary fw-bold rounded-1 text-center py-3">
+                    Browse Venues
+                  </Nav.Link>
+                  <Nav.Link href="/profile" className="bg-lighter text-primary fw-bold rounded-1 text-center py-3">
+                    My Profile
+                  </Nav.Link>
+                  <div className="mt-5 d-lg-none">
+                    <Nav.Link href="/create/venue" className="bg-danger text-white fw-bold rounded-1 text-center py-3">
+                      List your venue
+                    </Nav.Link>
+                  </div>
+                  <Nav.Link onClick={handleLogOut} className="mt-3 text-primary fw-bold border-0 rounded-1 text-center py-3">
+                    LOG OUT
+                  </Nav.Link>
+                </div>
+              )}
+              {!isLoggedIn && (
+                <div>
+                  <Nav.Link href="/login" className="bg-dark text-primary fw-bold rounded-1 text-center py-3">
+                    LOGIN
+                  </Nav.Link>
+                  <Nav.Link href="/register " className="bg-lighter text-primary fw-bold rounded-1 text-center py-3">
+                    REGISTER
+                  </Nav.Link>
+                </div>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
         <NavDropdown title={<FontAwesomeIcon icon={faUser} />} id="basic-nav-dropdown" className="d-none d-lg-flex justify-content-center align-items-center bg-light rounded-circle mt-3 text-primary" style={{ width: 50, height: 50 }}>
-          <NavDropdown.Item href="/">Home</NavDropdown.Item>
-          <NavDropdown.Item href="/profile">My Profile</NavDropdown.Item>
-          <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-          <NavDropdown.Item href="/register">Register</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item onClick={handleLogOut}>Log out</NavDropdown.Item>
+          {isLoggedIn && (
+            <div>
+              <NavDropdown.Item href="/">Home</NavDropdown.Item>
+              <NavDropdown.Item href="/profile">My Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={handleLogOut}>Log out</NavDropdown.Item>
+            </div>
+          )}
+          {!isLoggedIn && (
+            <div>
+              <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+              <NavDropdown.Item href="/register">Register</NavDropdown.Item>
+            </div>
+          )}
         </NavDropdown>
       </Container>
     </div>
